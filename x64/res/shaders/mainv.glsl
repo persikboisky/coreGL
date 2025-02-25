92f5537345e4bc20651a54202c2d6c1b1c5d558e
persikboisky <166322618+persikboisky@users.noreply.github.com>
2025-02-25 16:27:44 +0300

    openGL

--- x64/res/shaders/mainv.glsl
+++ x64/res/shaders/mainv.glsl
@@ -2,9 +2,11 @@
 
 layout (location = 0) in vec3 pos;
 
+uniform vec3 position;
 uniform mat4 view;
 uniform mat4 proj;
 
 void main() {
-	gl_Position = proj * view * vec4(pos, 1.0);
+	vec3 coord_vert = position + pos;
+	gl_Position = proj * view * vec4(coord_vert, 1.0);
 }
