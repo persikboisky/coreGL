eb148e6059016fe71f2f377e1e246fd4676a026c
persikboisky <166322618+persikboisky@users.noreply.github.com>
2025-03-01 22:57:13 +0300

    openGL

--- res/shaders/mainv.glsl
+++ res/shaders/mainv.glsl
@@ -2,9 +2,11 @@
 
 layout (location = 0) in vec3 position;
 layout (location = 1) in vec3 normal;
+layout (location = 2) in vec2 textCoord;
 
 out vec3 v_pos;
 out vec3 v_normal;
+out vec2 v_textCoord;
 
 uniform vec3 u_position;
 uniform mat4 view;
@@ -14,5 +16,6 @@ void main() {
 	vec4 pos = proj * view * vec4(u_position + position, 1.0);
 	v_pos = pos.xyz;
 	v_normal = normal;
+	v_textCoord = textCoord;
 	gl_Position = pos;
 }
