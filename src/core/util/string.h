5c5cefcd87f8b354f956334b981590774ba8f213
persikboisky <166322618+persikboisky@users.noreply.github.com>
2025-03-11 21:07:58 +0300

    openGL

--- src/core/util/string.h
+++
@@ -1,7 +0,0 @@
-#include <string>
-
-namespace string
-{
-	std::string searchFromString(std::string search_string, std::string string);
-	std::string searchFromFile(std::string search_string, const char* path);
-}
