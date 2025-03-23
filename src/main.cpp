c5fcc2de46fb4e77f26d964e8b007ef53c7fa1ae
persikboisky <166322618+persikboisky@users.noreply.github.com>
2025-03-23 23:29:41 +0300

    openGL

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,51 @@
 
 int main()
 {
+	core::Init();
 
+	try
+	{
+
+		Window window("opengl", 1280, 720);
+		window.setContext();
+
+		Cursor::custom_cursor* c = window.cursor->create("cursor.png", 0, 0);
+		c->use();
+
+		VAO vao({
+			-0.5, -0.5,
+			-0.5, 0.5,
+			0.5, 0.5,
+			0.5,-0.5
+		}, 2);
+		vao.addAttribute(0, 2, 0);
+
+		Shader s("v.glsl", "f.glsl");
+		s.use();
+
+		window.cursor->disableCursor(true);
+
+		while (true)
+		{
+			window.event->update();
+			if (window.event->getKey(K_ESCAPE))
+			{
+				break;
+			}
+
+			vao.draw(QUADS);
+			window.swapBuffers(true, 0, 0, 0, 1);
+		}
+
+	}
+	catch (const char* error)
+	{
+		core::Terminate();
+		return -1;
+	}
+
+	audio::source::DeleteALL();
+	audio::buffer::DeleteALL();
+	core::Terminate();
 	return 0;
 }
